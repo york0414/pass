6c7343ded70c4e3c9e38e4500ce09bfec3c568cc
York Liu <york0414@gmail.com>
2020-06-30 00:41:00 +0800

    chance i to chance

--- pass.py
+++ pass.py
@@ -1,32 +1,16 @@
 password = 'a123456'
-i = 3
-while i > 0 :
+chance = 3
+while chance > 0 :
+	chance = chance - 1
 	pwd = input ('請輸入密碼: ')
 	if pwd == password:
 		print ('登入成功')
 		break
 	else:
-		i = i - 1
-		print ('密碼錯誤！ 還有', i ,'次機會！')
-		
+		print ('密碼錯誤！')
+		if chance > 0 :
+		     print ('還有', chance ,'次機會！')
+		else:
+			 print('沒機會嘗試了！帳號以被鎖了')
 
-
-
-
-
-
-
-
-
-password = 'a123456'
-i = 3
-while True :
-	pwd = input ('請輸入密碼: ')
-	if pwd == password:
-		print ('登入成功')
-		break
-	else:
-		i = i - 1
-		print ('密碼錯誤！ 還有', i ,'次機會！')
-		if i == 0:
-		     break
+	
