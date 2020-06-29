26b648e3ebd92d2811597da34481a0f4c3213378
York Liu <york0414@gmail.com>
2020-06-30 00:22:30 +0800

    change to i>0

--- pass.py
+++ pass.py
@@ -1,5 +1,25 @@
 password = 'a123456'
 i = 3
+while i > 0 :
+	pwd = input ('請輸入密碼: ')
+	if pwd == password:
+		print ('登入成功')
+		break
+	else:
+		i = i - 1
+		print ('密碼錯誤！ 還有', i ,'次機會！')
+		
+
+
+
+
+
+
+
+
+
+password = 'a123456'
+i = 3
 while True :
 	pwd = input ('請輸入密碼: ')
 	if pwd == password:
